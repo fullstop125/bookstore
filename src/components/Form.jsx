bead4ac89b8e03874962b56131a473bf71fc0ddc
fullstop125 <hassanmoses125@gmail.com>
2022-09-15 23:42:01 +0300

    fix linters

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,8 +3,8 @@ import '../assets/styles/form.css';
 
 const Form = () => (
   <form>
-    <input type="text" placeholder='Title' />
-    <input type="text" placeholder='Author' />
+    <input type="text" placeholder="Title" />
+    <input type="text" placeholder="Author" />
     <button type="submit">Submit</button>
   </form>
 );
